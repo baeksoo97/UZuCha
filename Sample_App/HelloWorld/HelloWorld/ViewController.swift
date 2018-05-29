74e091fb819f61c4928862b5b9a2dd49761de5d3
Superwonjune <avgnrolfe@naver.com>
2018-05-29 18:05:32 +0900

    search by ID 예시 추가

--- Sample_App/HelloWorld/HelloWorld/ViewController.swift
+++ Sample_App/HelloWorld/HelloWorld/ViewController.swift
@@ -13,10 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 주의사항
+        // 아래 함수들은 비동기 함수라서 실행 순서를 보장할 수 없음 **
+        
+        
         // 사용 예시
         //
         // 1. 저장되어있는 모든 DB 순회하면서 모든 데이터 불러오기
-        JSONParking.getAllParkingsJSON(commands: "api/parkings") { (results:[JSON_Parking_type]) in
+        JSONParking.getAllParkingsJSON() { (results:[JSON_Parking_type]) in
             // results: JSON 타입의 배열
             for result in results {
                 
@@ -29,6 +33,17 @@ class ViewController: UIViewController {
                 print("\n")
             }
         }
+        
+        
+        
+        // 2. id로 검색해서 해당 id의 json 데이터 하나 불러오기
+        // id parameter 필요
+        JSONParking.getParkingByID(id: "5b082d96791d9b0d7263c93c") { (result:JSON_Parking_type) in
+            // result : JSON type
+            print("\(result)")
+            print("\(result.building_name)\n")
+        }
+        
         
         
     }
