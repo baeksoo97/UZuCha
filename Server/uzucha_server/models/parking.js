32f7601ffbb8d35610001559aaa974a08c1db4e2
Superwonjune <avgnrolfe@naver.com>
2018-05-31 16:47:12 +0900

    DB model Application에 맞게 수정

--- Server/uzucha_server/models/parking.js
+++ Server/uzucha_server/models/parking.js
@@ -11,44 +11,30 @@ var ParkingSchema = new Schema({
         markerName: String
     },
 
-    building_name: { type: String, required: true},
-    building_image_dir: [ {type: String} ], 
-    building_address: { type: String, required: true},
+    building: {
+        building_name: { type: String, required: true},
+        building_image_dir: [ {type: String} ], 
+        building_address: { type: String, required: true}
+    },
     
     // park_owner
-    owner_name: { type: String, required: true},
-    owner_mail_address: String,
-    owner_phone_number: String,
-    
-    price: { type: String, required: true},
-    availabe_time: String,
-    is_favorite: { type: Boolean, default: false},
+    owner : {
+        owner_name: { type: String, required: true},
+        owner_mail_address: String,
+        owner_phone_number: String
+    },
 
+    detail : {
+        capacity: Number,
+        floor: Number,
+        availabe_time: String,
+    },
+
+    price: { type: String, required: true},
+    is_favorite: { type: Boolean, default: false},
     owner_comment: String,
 
     created_at: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('parking', ParkingSchema);
-
-/*
-
-건물 사진
-건물 이름
-건물 주소
-
-건물주
-    (이름) (key)
-    (메일주소)
-    (전화번호)
-
-건물 가격 (string)
-가능한 시간대 (string)
-
-관심목록인지 아닌지 (boolean)
-
-상세 설명
-담당자 한마디
-
-등록 시간
-*/
